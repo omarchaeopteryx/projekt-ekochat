a400f40ee465fcd2137f2d98f28a53bcb06aa43c
omarchaeopteryx <omaralimalik@gmail.com>
2017-04-20 14:41:57 -0700

    Add jQuery clock interactivity effect

--- web-dowhop/scripts/jquery.js
+++ web-dowhop/scripts/jquery.js
@@ -1,8 +1,21 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    $('#new-chat-input-when-time').hide()
+    // console.log( "ready!" ); // <-- Debugging
+
+    // Add the interactive calendar selection:
     $( "#new-chat-input-when-date" ).datepicker({
                 showOn: "button",
                 buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
                 buttonImageOnly: true
             });
+
+    // Add the interactive clock selection:
+    $('#new-chat-input-when-time-icon').on('click', function() {
+      console.log('you clocked a clock!');
+      $('#new-chat-input-when-time').show();
+    })
 });
+
+// See also: https://weareoutman.github.io/clockpicker/jquery.html
+// See also: https://www.npmjs.com/package/material-datetime-picker
